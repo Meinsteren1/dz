dc4e728fadb3c894181866fc2e7d9a8f9e297ced
meinsteren <meinsteren@gmail.com>
2024-03-15 18:47:01 +0300

    comit

---
+++ zadanie 7.1.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    val solution = Solution()
+    solution.plusOne(IntArray(9,9,9))
+    println()
+}
---
+++ zadanie 7.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun plusOne (digits: IntArray): IntArray {
+        for (i in digits.lastIndex downTo 0) {
+            if (digits[i] < 9) {
+                digits[i]++
+                return digits
+            } else {
+                digits[i] = 0
+            }
+        }
+        return digits
+    }
+}
