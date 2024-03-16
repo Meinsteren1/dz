e1b9fa436b86aa1aa7bf2c2cd46679dfd2145614
Meinsteren1 <160304961+Meinsteren1@users.noreply.github.com>
2024-03-16 14:50:58 +0300

    Add files via upload

---
+++ zadanie 4.kt
@@ -0,0 +1,12 @@
+fun printResult(userChoice: String, gameChoice: String) {
+    val result: String
+// Определить результат
+    if (userChoice == gameChoice) result = "Ничья!"
+    else if ((userChoice == "Камень" && gameChoice == "Ножницы") ||
+        (userChoice == "Бумага" && gameChoice == "Камень") ||
+        (userChoice == "Ножницы" && gameChoice == "Бумага"))
+        result = "Вы выиграли!"
+    else result = "Вы проиграли!"
+// Вывести результат
+    println("Вы выбрали $userChoice. Я выбрал $gameChoice. $result")
+}
