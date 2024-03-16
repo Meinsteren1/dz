9e97b83cde38d3b62f33d0b7cc83b90ac51a0b0c
Meinsteren1 <160304961+Meinsteren1@users.noreply.github.com>
2024-03-16 14:47:48 +0300

    Add files via upload

---
+++ zadanie2.kt
@@ -0,0 +1,8 @@
+fun main(args:Array<String>) {
+    var x=1
+    while (x<4){
+        print(if (x==1)"Уab" else print("ba"))
+        x=+1
+    }
+    if (x==2) println("Do")
+    }
---
+++ zadnie 4.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val firemen= arrayOf("Pugh", "Martin", "Barney McGrew", "Cuthbert", "Dibble", "Grub")
+    var firemanNo=0;
+    while (firemanNo<6) {
+        println("Fireman number $firemanNo is ${firemen[firemanNo]}")
+        firemanNo=firemanNo+1
+    }
+
+}
