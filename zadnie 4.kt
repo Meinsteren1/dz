d35285426d6d90a7c3b8120fa24905ed979dd8bf
Meinsteren1 <160304961+Meinsteren1@users.noreply.github.com>
2024-04-06 15:15:55 +0300

    Delete zadnie 4.kt

--- zadnie 4.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val firemen= arrayOf("Pugh", "Martin", "Barney McGrew", "Cuthbert", "Dibble", "Grub")
-    var firemanNo=0;
-    while (firemanNo<6) {
-        println("Fireman number $firemanNo is ${firemen[firemanNo]}")
-        firemanNo=firemanNo+1
-    }
-
-}
