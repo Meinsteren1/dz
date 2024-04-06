4e960c16c59f840092c6ee2aab5ad19b48c666f1
Meinsteren1 <160304961+Meinsteren1@users.noreply.github.com>
2024-04-06 15:03:04 +0300

    Delete zadani3.kt

--- zadani3.kt
+++
@@ -1,8 +0,0 @@
-fun main(args:Array<String>) {
-    val hobbits = arrayOf("Frodo","Sam","Merry", "Pippin")
-    var x=0;
-    while (x<5) {
-        println("${hobbits[x]} is a good Hobbit name")
-        x=x+1
-    }
-}
